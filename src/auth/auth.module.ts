45098986cd63b84f1f992a24eff5e0d8c57b04ce
iyj6707 <yeonjae@scatterlab.co.kr>
2022-08-25 11:34:47 +0900

    feat: yaml 파일 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,9 @@ import { PrismaService } from './../prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   controllers: [AuthController],
@@ -13,9 +16,9 @@ import { JwtStrategy } from './jwt.strategy';
     PrismaService,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'secret1234',
+      secret: jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600
+        expiresIn: jwtConfig.expiresIn
       }
     }),
   ],
